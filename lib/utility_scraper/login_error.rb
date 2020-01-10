6ddfa29785cfe152436305eaccb523161bd18059
Komaron <kjames953@gmail.com>
2020-01-09 19:21:40 -0500

    Updating error verbiage

--- lib/utility_scraper/login_error.rb
+++ lib/utility_scraper/login_error.rb
@@ -1,6 +1,6 @@
 module UtilityScraper
   class LoginError < StandardError
-    def initialize(msg = 'Error logging in, please check your credentials and try again')
+    def initialize(msg = 'Error logging in, please check your credentials and try again!')
       super
     end
   end
